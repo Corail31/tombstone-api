720cb79f07c75ee94b37a2f47caf6032ae36db4f
Corail31 <m.pietquin@free.fr>
2019-09-03 13:34:05 +0200

    update api 1.14.4

--- TombstoneAPIProps.java
+++ TombstoneAPIProps.java
@@ -3,7 +3,7 @@ package ovh.corail.tombstone.api;
 @SuppressWarnings({ "unused" })
 public class TombstoneAPIProps {
     public static final String OWNER = "tombstone";
-    public static final String OWNER_VERSION = "3.7.5";
+    public static final String OWNER_VERSION = "4.1.1";
     public static final String API = "tombstone-api";
-    public static final String VERSION = "1.0.4"; // version of the api
+    public static final String VERSION = "1.0.5"; // version of the api
 }
